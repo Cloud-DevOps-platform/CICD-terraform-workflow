318f74162da1df74e9a246c596f5387ac79c4e3c
pmkumar1 <12g31a0227@gmail.com>
2024-03-23 14:41:55 +0530

    files modified

--- sourcecode/terraform/webapp.tf
+++ sourcecode/terraform/webapp.tf
@@ -1,17 +1,17 @@
-resource "azurerm_service_plan" "cdxserviceplan" {
-  name                = "hdforwebapp"
-  resource_group_name = azurerm_resource_group.CDX-platform.name
-  location            = azurerm_resource_group.CDX-platform.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
-}
+# resource "azurerm_service_plan" "cdxserviceplan" {
+#   name                = "hdforwebapp"
+#   resource_group_name = azurerm_resource_group.CDX-platform.name
+#   location            = azurerm_resource_group.CDX-platform.location
+#   os_type             = "Linux"
+#   sku_name            = "P1v2"
+# }
 
-resource "azurerm_linux_web_app" "nodejs_web_app" {
-  name                = "webppcdx"
-  resource_group_name = azurerm_resource_group.CDX-platform.name
-  location            = azurerm_resource_group.CDX-platform.location
-  service_plan_id     = azurerm_service_plan.cdxserviceplan.id
+# resource "azurerm_linux_web_app" "nodejs_web_app" {
+#   name                = "webppcdx"
+#   resource_group_name = azurerm_resource_group.CDX-platform.name
+#   location            = azurerm_resource_group.CDX-platform.location
+#   service_plan_id     = azurerm_service_plan.cdxserviceplan.id
 
-  site_config {}
+#   site_config {}
 
-}
+# }
