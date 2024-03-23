fa35e0995453093283cbd99965f3f8e02e6e00fe
pmkumar1 <12g31a0227@gmail.com>
2024-03-23 14:30:22 +0530

    files modified

--- sourcecode/terraform/backend.tf
+++ sourcecode/terraform/backend.tf
@@ -1,12 +1,12 @@
 # creating the terraform backend to store the state file
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "CDX-platform"
-    storage_account_name = "InfraCDX-backup"
-    container_name       = "cdxtffile-container"
-    key                  = "backup.tfstate"
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "CDX-platform"
+#     storage_account_name = "InfraCDX-backup"
+#     container_name       = "cdxtffile-container"
+#     key                  = "backup.tfstate"
 
 
-  }
-}
+#   }
+# }
