d9695b679c3dd4e492e2d3b5fd7d67410e79345d
pmkumar1 <12g31a0227@gmail.com>
2024-03-23 14:39:39 +0530

    files modified

--- sourcecode/terraform/backend.tf
+++ sourcecode/terraform/backend.tf
@@ -5,7 +5,7 @@ terraform {
     resource_group_name  = "CDX-platform"
     storage_account_name = "infraCDX-backup"
     container_name       = "cdxtffile-container"
-    key                  = "backup.tfstate"
+    key                  = "terraformconfig.tfstate"
 
 
   }
