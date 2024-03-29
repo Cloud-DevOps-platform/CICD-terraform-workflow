ff7c43fb285e11cc065e37b407d96783dfd4ccf3
pmkumar1 <12g31a0227@gmail.com>
2024-03-29 05:11:47 -0700

    files added

--- sourcecode/terraform/backend.tf
+++ sourcecode/terraform/backend.tf
@@ -1,11 +1,11 @@
 # creating the terraform backend to store the state file
 
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "prod-deployment"
-#     storage_account_name = "remotebackendstate"
-#     container_name       = "storagecontinerstate"
-#     key                  = "terraformconfig.tfstate"
-#   }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "prod-deployment"
+    storage_account_name = "remotebackendstate"
+    container_name       = "storagecontinerstate"
+    key                  = "terraformconfig.tfstate"
+  }
 
-# }
+}
